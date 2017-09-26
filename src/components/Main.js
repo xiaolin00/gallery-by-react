4483706bc371c4ac86169bdc6ad6e03e9bcb77be
xiaolin <15032342810@163.com>
2017-09-26 16:14:39 +0800

    first

---
+++ src/components/Main.js
@@ -0,0 +1,40 @@
+require('normalize.css/normalize.css');
+require('styles/App.scss');
+
+import React from 'react';
+
+// let yeomanImage = require('../images/yeoman.png');
+
+//获取图片的相关数据
+var imageDatas = require('../data/imageDatas.json');
+
+//利用自执行函数，将图片信息转成图片URL路径信息
+    imageDatas = (function genImageURL (imageDatasArr) {
+        for (var i = 0, j = imageDatasArr.length; i < j; i++){
+            var singleImageData  = imageDatasArr[i];
+                singleImageData.imageURL = require('../images/'+
+                singleImageData.fileName);
+            imageDatasArr[i] = singleImageData;
+        }
+        return imageDatasArr;
+    })(imageDatas);
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+        <section className="stage">
+            <section className="img-sec">
+
+            </section>
+            <nav className="controer-nav">
+
+            </nav>
+        </section>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
